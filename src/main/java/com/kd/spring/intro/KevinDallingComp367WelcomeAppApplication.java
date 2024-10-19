4706c056bb9d3207d8c8cabfa140f4fb2a28cbae
Kevin Dalling <kdalling@my.centennialcollege.ca>
2024-10-19 15:36:31 -0400

    Initial commit

---
+++ src/main/java/com/kd/spring/intro/KevinDallingComp367WelcomeAppApplication.java
@@ -0,0 +1,13 @@
+package com.kd.spring.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinDallingComp367WelcomeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevinDallingComp367WelcomeAppApplication.class, args);
+	}
+
+}
